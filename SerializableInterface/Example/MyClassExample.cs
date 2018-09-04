b9c8e1b612bf641a8f3aad7240107d93226af6c7
Ghat Smith <ghatsmith@gmail.com>
2018-09-04 19:47:47 +0100

    Update MyClassExample.cs
    Add array and list fields for to improve example.

--- SerializableInterface/Example/MyClassExample.cs
+++ SerializableInterface/Example/MyClassExample.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
 
 
 namespace OddTales.Framework.Core.Other.Examples
@@ -6,5 +7,7 @@ namespace OddTales.Framework.Core.Other.Examples
     public class MyClassExample : MonoBehaviour
     {
         [SerializeField] private SerializedInterfaceExample myInterface;
+        [SerializeField] private List<SerializedInterfaceExample> listOfInterfaces;
+        [SerializeField] private SerializedInterfaceExample[] arrayOfInterfaces;
     }
 }
